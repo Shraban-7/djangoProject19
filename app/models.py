2b56abb7e944b8214f6bdc6f0d80933b04982a18
shraban-7 <shakuatshraban@gmail.com>
2020-11-20 18:56:24 +0600

    add

--- app/models.py
+++ app/models.py
@@ -2,32 +2,23 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
-    TEN = '10'
-    NINE = '9'
-    EIGHT = '8'
-    SEVEN = '7'
-    SIX = '6'
-    FIVE = '5'
-    FOUR = '4'
-    THREE = '3'
-    TWO = '2'
-    ONE = '1'
-    year_in_scholl_choice = [(TEN , '10'),
-                             (NINE ,'9'),
-                             (EIGHT , '8'),
-                             (SEVEN , '7'),
-                             (SIX , '6'),
-                             (FIVE , '5'),
-                             (FOUR,'4'),
-                             (THREE, '3'),
-                             (TWO,'2'),
-                             (ONE, '1'),
-                             ]
+
+    year_in_scholl_choice =( ('TWELVE','12'), ('ELEVEN','11'),
+                             ('TEN' , '10'),
+                             ('NINE' ,'9'),
+                             ('EIGHT' , '8'),
+                             ('SEVEN' , '7'),
+                             ('SIX' , '6'),
+                             ('FIVE' , '5'),
+                             ('FOUR','4'),
+                             ('THREE', '3'),
+                             ('TWO','2'),
+                             ('ONE', '1'))
     medium_choice = (('English','English'),
                      ('Bangla','Bangla'))
     title = models.CharField(max_length=255)
     content = models.TextField()
-    class_school = models.CharField(max_length=2,choices=year_in_scholl_choice,default=ONE)
+    class_school = models.CharField(max_length=15,choices=year_in_scholl_choice,default='ONE')
     medium = models.CharField(max_length=50,choices=medium_choice,default='Bangla')
     salary = models.DecimalField(max_digits=7,decimal_places=2)
     location = models.CharField(max_length=250,default='Dhaka')
