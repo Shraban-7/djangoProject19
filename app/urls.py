0f6240178441998c0bc350e4779cad74e81db048
shraban-7 <shakuatshraban@gmail.com>
2020-11-21 16:17:48 +0600

    Merge remote-tracking branch 'origin/master'

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import Home
+from .views import Home,PostCreateView,PostList,PostDetail
 
 urlpatterns = [
 
     path('',Home.as_view(),name = 'home'),
+    path('create/',PostCreateView.as_view(),name = 'create'),
+    path('list/',PostList.as_view(),name = 'list'),
+    path('detail/<int:pk>',PostDetail.as_view(),name = 'detail'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
-
-
+from django.shortcuts import render,redirect
+from django.views.generic import TemplateView,CreateView,ListView,DetailView,DeleteView,UpdateView
+from .models import Post
+from .forms import PostForm
 # Create your views here.
 class Home(TemplateView):
     template_name = 'tuition/home.html'
@@ -9,3 +9,23 @@ class Home(TemplateView):
         contex = super().get_context_data(**kwargs)
         contex ['msg'] = 'Welcome to our website '
         return contex
+
+class PostCreateView(CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'tuition/create.html'
+    success_url = '/'
+    # redirect('home')
+
+class PostList(ListView):
+    model = Post
+    template_name = 'tuition/list.html'
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'tuition/detail.html'
+
+class PostUpdate(UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'tuition/update.html'
